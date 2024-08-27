28a586f258c3082b079dddf56c74da18303ec13b
Mourya Rada <radamourya7@gmail.com>
2024-08-27 20:27:31 +0530

    Create Q2.cpp

---
+++ Q2.cpp
@@ -0,0 +1,11 @@
+Ques-02 Write C++ program that take array from the user and sort the array using insertion sort algorithm.
+
+Input:
+N = 5
+arr[] = {4, 1, 3, 9, 7}
+
+Output:
+1 3 4 7 9
+
+CODE:=
+
